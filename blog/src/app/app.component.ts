7abfbd4899856492af67026780ec5ac18d9ecb57
shahnawazali786 <ejnawazali@gmail.com>
2023-05-19 12:06:38 +0530

    submit

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,15 +7,27 @@ import { FormControl, FormsModule, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
 
-  title = 'blog';
+  title = 'Login Form  ';
   loginForms = new FormGroup
     ({
-      users: new FormControl(''),
-      password: new FormControl('')
+      users: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
 
     })
   loginUser() {
     console.warn(this.loginForms.value)
   }
+
+
+  get user() {
+    return this.loginForms.get('user')
+  }
+
+
+  get password() {
+    return this.loginForms.get('password')
+  }
+
+
 
 }
